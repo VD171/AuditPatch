637a5d5fe3e72c53d7e735a66b02a502f9547d17
VD $ VD171 @ Priv8 <80125987+VD171@users.noreply.github.com>
2025-08-27 00:10:05 -0300

    Update hook.c

--- module/src/main/cpp/hook.c
+++ module/src/main/cpp/hook.c
@@ -20,48 +20,30 @@ static bool is_in_quotes(const char *str, const char *pos) {
 }
 
 static int my_vasprintf(char **strp, const char *fmt, va_list ap) {
-    // https://cs.android.com/android/platform/superproject/main/+/main:system/logging/logd/LogAudit.cpp;l=210
     int result = old_vasprintf(strp, fmt, ap);
-
     if (result > 0 && *strp) {
-        const char *target_context = "tcontext=u:r:kernel:s0";
-
         const char *source_contexts[] = {
             "tcontext=u:r:su:s0",
             "tcontext=u:r:magisk:s0",
             "tcontext=u:r:proc_modules:s0",
             "tcontext=u:object_r:proc_modules:s0"
         };
-
         size_t source_contexts_len = sizeof(source_contexts) / sizeof(source_contexts[0]);
-
-        size_t target_len = strlen(target_context);
-
         for (size_t i = 0; i < source_contexts_len; i++) {
             const char *source = source_contexts[i];
             char *pos = strstr(*strp, source);
-
             if (pos && !is_in_quotes(*strp, pos)) {
                 size_t source_len = strlen(source);
-                size_t extra_space = (target_len > source_len) ? (target_len - source_len) : 0;
-
-                // Reverse double space in case
-                char *new_str = malloc(result + 2 * extra_space + 1);
+                char *new_str = malloc(result + 1);
                 strcpy(new_str, *strp);
                 pos = new_str + (pos - *strp);
-
-                if (source_len != target_len) {
-                    memmove(pos + target_len, pos + source_len, strlen(pos + source_len) + 1);
-                }
-                memcpy(pos, target_context, target_len);
-
+                memmove(pos, pos + source_len, strlen(pos + source_len) + 1);
                 free(*strp);
                 *strp = new_str;
                 return (int)strlen(new_str);
             }
         }
     }
-
     return result;
 }
 
